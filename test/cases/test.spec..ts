79773d054beeff72f173c273891c4ed1f332a7ba
Ori Geva <origeva636@gmail.com>
2021-09-07 23:05:45 +0300

    Fixed test to be done only when pipe closes

--- test/cases/test.spec..ts
+++ test/cases/test.spec..ts
@@ -1,4 +1,4 @@
-import { Writable } from 'stream'
+import { pipeline, Writable } from 'stream'
 import fs from 'fs'
 import sbdl, { url } from '../config'
 
@@ -7,7 +7,12 @@ describe('Execute sbdl', () => {
 	before(() => {
 		destination = fs.createWriteStream('./test/test.mp3')
 	})
-	it('should download video and pipe it to a file.', async () => {
-		;(await sbdl(url, 'audio', 'mp3')).pipe(destination)
+	it('should download video and pipe it to a file.', (done) => {
+		sbdl(url, 'audio', 'mp3').then((video) => {
+			video.pipe(destination)
+			video.once('close', () => {
+				done()
+			})
+		})
 	})
 })
